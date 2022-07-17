2f6ac970661366362df96ed9d32c30f1d162d429
bigcoqui <102246267+bigcoqui@users.noreply.github.com>
2022-07-17 17:20:36 -0300

    update CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -159,6 +159,11 @@ class CreditsState extends MusicBeatState
 		bg.color = getCurrentBGColor();
 		intendedColor = bg.color;
 		changeSelection();
+
+		#if android
+		addVirtualPad(UP_DOWN, A_B);
+		#end
+
 		super.create();
 	}
 
